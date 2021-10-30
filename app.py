340cd9408931241fa586ee621d1ade85079e3fcc
MTL1991 <manuel.toro.legaz@gmail.com>
2021-10-30 12:16:12 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,173 @@
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+import numpy as np
+from dash.dependencies import Output, Input
+
+data = pd.read_csv("avocado.csv")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Analisis del aguacate"
+server = app.server
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(
+                    children="Analisis del Aguacate", className="header-title"
+                ),
+                html.P(
+                    children="Analizar el comportamiento del precio del aguacate"
+                             " y el numero de aguacates vendidos en US"
+                             " entre 2015 y 2018",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="filtro-region",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in np.sort(data.region.unique())
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Type", className="menu-title"),
+                        dcc.Dropdown(
+                            id="filtro-tipo",
+                            options=[
+                                {"label": avocado_type, "value": avocado_type}
+                                for avocado_type in data.type.unique()
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                            ),
+                        dcc.DatePickerRange(
+                            id="rango-fecha",
+                            min_date_allowed=data.Date.min().date(),
+                            max_date_allowed=data.Date.max().date(),
+                            start_date=data.Date.min().date(),
+                            end_date=data.Date.max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="grafica-precio", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="grafica-volumen", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+@app.callback(
+    [Output("grafica-precio", "figure"), Output("grafica-volumen", "figure")],
+    [
+        Input("filtro-region", "value"),
+        Input("filtro-tipo", "value"),
+        Input("rango-fecha", "start_date"),
+        Input("rango-fecha", "end_date"),
+    ],
+)
+def update_charts(region, avocado_type, start_date, end_date):
+    mask = (
+        (data.region == region)
+        & (data.type == avocado_type)
+        & (data.Date >= start_date)
+        & (data.Date <= end_date)
+    )
+    filtered_data = data.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Avocados Sold",
+                "x": 0.05,
+                "xanchor": "left"
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
